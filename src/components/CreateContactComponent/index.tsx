10ee3e012a183b48161bc540d7a06b253876dd5a
MACBOOK <ben10a2992@gmail.com>
2021-10-06 23:59:45 +0200

    Add country picker library to project

--- src/components/CreateContactComponent/index.tsx
+++ src/components/CreateContactComponent/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {Image, Text, View} from 'react-native';
 import CustomButton from '../common/CustomButton';
 import Input from '../common/Input';
@@ -6,7 +6,19 @@ import styles from './styles';
 import CountryPicker from 'react-native-country-picker-modal';
 import {DEFAULT_IMAGE_URI} from '../../constants/general';
 
-const CreateContactComponent = () => {
+interface Prop {
+  form?: {};
+  setForm?: any;
+  onChangeText?: ({}) => void;
+  onSubmit?: () => void;
+}
+
+const CreateContactComponent: FC<Prop> = ({
+  form,
+  setForm,
+  onSubmit,
+  onChangeText,
+}) => {
   return (
     <View style={styles.container}>
       <Image source={{uri: DEFAULT_IMAGE_URI}} style={styles.defaultImage} />
@@ -14,30 +26,41 @@ const CreateContactComponent = () => {
       <Input
         label="First name"
         placeholder="Enter first name"
-        onChangeText={() => {}}
+        onChangeText={value => {
+          onChangeText({name: 'firstName', value: value});
+        }}
       />
       <Input
         label="Last name"
         placeholder="Enter last name"
-        onChangeText={() => {}}
+        onChangeText={value => {
+          onChangeText({name: 'lastName', value: value});
+        }}
       />
       <Input
         label="Phone number"
         placeholder="Enter phone number"
-        onChangeText={() => {}}
+        onChangeText={value => {
+          onChangeText({name: 'phoneNumber', value: value});
+        }}
         icon={
           <CountryPicker
-            countryCode="EG"
             withFilter
             withFlag
+            countryCode={form.countryCode || undefined}
+            withCallingCodeButton
             withCountryNameButton={false}
             withEmoji
-            onSelect={() => {}}
+            onSelect={v => {
+              const phoneCode = v.callingCode[0];
+              const cCode = v.cca2;
+              setForm({...form, phoneCode, countryCode: cCode});
+            }}
           />
         }
         iconPosition="left"
       />
-      <CustomButton title="Submit" onSubmit={() => {}} primary />
+      <CustomButton title="Submit" onSubmit={onSubmit} primary />
     </View>
   );
 };
--- src/screens/CreateContact/index.tsx
+++ src/screens/CreateContact/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CreateContactComponent from '../../components/CreateContactComponent';
 
 const CreateContact = () => {
-  return <CreateContactComponent />;
+  const [form, setForm] = useState({});
+  const onChangeText = ({name, value}: any) => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = () => {
+    console.log(1111, '1111');
+    console.log('Form', form);
+  };
+
+  return (
+    <CreateContactComponent
+      form={form}
+      setForm={setForm}
+      onChangeText={onChangeText}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default CreateContact;
