360b96376b6e964f9b366d06484e6bbcbdf528ba
MACBOOK <ben10a2992@gmail.com>
2021-10-13 18:56:45 +0200

    Managing user preference

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useEffect, useState} from 'react';
 import SettingsComponent from '../../components/SettingsComponent';
 
 const Settings = () => {
+  const [email, setEmail] = useState();
+
   const settingsOptions = [
     {title: 'My Info', subTitle: 'Setup your profile', onPress: () => {}},
     {title: 'Accounts', subTitle: null, onPress: () => {}},
     {
       title: 'Default account for new contacts',
-      subTitle: 'Aabb@io.com',
+      subTitle: email,
       onPress: () => {},
     },
     {title: 'Contacts to display', subTitle: 'All contacts', onPress: () => {}},
@@ -22,6 +25,16 @@ const Settings = () => {
     {title: 'Blocked numbers', subTitle: null, onPress: () => {}},
     {title: 'About RNContacts', subTitle: null, onPress: () => {}},
   ];
+
+  const getSettings = async () => {
+    const user: any = await AsyncStorage.getItem('user');
+    setEmail(JSON.parse(user).email);
+  };
+
+  useEffect(() => {
+    getSettings();
+  }, []);
+
   return <SettingsComponent options={settingsOptions} />;
 };
 
