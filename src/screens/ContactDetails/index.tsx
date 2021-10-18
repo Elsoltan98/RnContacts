1d9022a7bc08cf6647ace2f953e73c5e07bb9d47
MACBOOK <ben10a2992@gmail.com>
2021-10-18 16:46:14 +0200

    Add header options on ContactsDetails screen

--- src/screens/ContactDetails/index.tsx
+++ src/screens/ContactDetails/index.tsx
@@ -1,13 +1,45 @@
-import {useRoute} from '@react-navigation/native';
-import React from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import colors from '../../colors';
+import Icon from '../../components/common/Icon';
 import ContactsDetailsComponent from '../../components/ContactsDetailsComponent';
 
 const ContactDetails = () => {
+  const {setOptions} = useNavigation();
   const {
     params: {item},
   }: any = useRoute();
 
-  console.log('item', item);
+  useEffect(() => {
+    if (item) {
+      setOptions({
+        title: `${item.first_name} ${item.last_name}`,
+        headerRight: () => {
+          return (
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity>
+                <Icon
+                  name={item.is_favorite ? 'star' : 'staro'}
+                  size={21}
+                  color={colors.primary}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={{paddingLeft: 10}}>
+                <Icon
+                  type="MaterialCommunityIcons"
+                  name="delete"
+                  size={21}
+                  color={colors.primary}
+                />
+              </TouchableOpacity>
+            </View>
+          );
+        },
+      });
+    }
+  }, [item]);
+
   return <ContactsDetailsComponent contact={item} />;
 };
 
